2900f7ab6ca3b2ce9e7bc4760531e185734dcb21
PhKhang <trannguyenphuckhang12@gmail.com>
2021-07-13 14:58:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setcolour(){
     document.title="Another \u30DF\u30AB\u30C5\u30ADBigWave~"
-    setTimeout(function () {document.title="Another BigWave~";},90);
+    setTimeout(function () {document.title="Another BigWave~";},200);
     
 }
 var thing = setInterval(setcolour, 5000);
